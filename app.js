4c018c50f8fcae5d3f6e4731952ccf3a96069e53
Kristoporus <nkristoporus@gmail.com>
2018-06-29 00:07:46 +0800

    Initializing express and moving html files to ejs

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-	res.send("Hi there");
+	res.render("home.ejs");
+});
+
+app.get("/post", function(req, res){
+	res.render("post.ejs");
 });
 
 app.listen(3000, process.env.IP);
