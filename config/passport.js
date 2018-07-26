5cd27aefcfac86b2950ecf86a94d620fefe51aa3
Matthew Alexander <alexandermatthew12@gmail.com>
2018-07-26 14:05:25 +0700

    Fixed login-page problem

--- config/passport.js
+++ config/passport.js
@@ -54,18 +54,32 @@ module.exports = passport => {
     });
   }));
 
-  // login
   passport.use('local-login', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  (req, email, password, done) => {
-    User.findOne({ 'local.email': email }, (err, user) => {
-      if (err) return done(err);
-      if (!user) return done(null, false, req.flash('loginMessage', 'No user found.'));
-    })
-  }
-))
+      // by default, local strategy uses username and password, we will override with email
+      usernameField : 'email',
+      passwordField : 'password',
+      passReqToCallback : true // allows us to pass back the entire request to the callback
+  }, (req, email, password, done) => { // callback with email and password from our form
+
+      // find a user whose email is the same as the forms email
+      // we are checking to see if the user trying to login already exists
+      User.findOne({ 'local.email' :  email }, (err, user) => {
+          // if there are any errors, return the error before anything else
+          if (err)
+              return done(err);
+
+          // if no user is found, return the message
+          if (!user)
+              return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+          // if the user is found but the password is wrong
+          if (!user.validPassword(password))
+              return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+          // all is well, return successful user
+          return done(null, user);
+      });
+
+  }));
 
 };
